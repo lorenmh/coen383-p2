0ef83419a653ad72dfcc1a8252085a5a4064dcde
yzhang <azraelexia@gmail.com>
2017-10-03 15:32:50 -0700

    added scheduler declaration

---
+++ Scheduler.c
@@ -0,0 +1,25 @@
+#include "Scheduler.h"
+
+void fcfs(process_queue_t *pq, history_t *h) {
+
+}
+
+void sjf(process_queue_t *pq, history_t *h) {
+
+}
+
+void srt(process_queue_t *pq, history_t *h) {
+
+}
+
+void rr(process_queue_t *pq, history_t *h) {
+
+}
+
+void hpf_npe(process_queue_t *pq, history_t *h) {
+
+}
+
+void hpf_pe(process_queue_t *pq, history_t *h) {
+
+}
---
+++ Scheduler.h
@@ -0,0 +1,60 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+#include "Process.h"
+#include "Output.h"
+
+
+/**
+ * First Come First Serve scheduler
+ *
+ * @param pq pointer to the process queue
+ * @param h pointer to the result
+ */
+void fcfs(process_queue_t *pq, history_t *h);
+
+
+/**
+ * Shortest Job First scheduler
+ *
+ * @param pq pointer to the process queue
+ * @param h pointer to the result
+ */
+void sjf(process_queue_t *pq, history_t *h);
+
+
+/**
+ * Shortest Remaining Time First scheduler
+ *
+ * @param pq pointer to the process queue
+ * @param h pointer to the result
+ */
+void srt(process_queue_t *pq, history_t *h);
+
+/**
+ * Round Robin scheduler
+ *
+ * @param pq pointer to the process queue
+ * @param h pointer to the result
+ */
+void rr(process_queue_t *pq, history_t *h);
+
+/**
+ * Highest Priority First, non-preemptive scheduler
+ *
+ * @param pq pointer to the process queue
+ * @param h pointer to the result
+ */
+void hpf_npe(process_queue_t *pq, history_t *h);
+
+/**
+ * Highest Priority First, preemptive scheduler
+ *
+ * @param pq pointer to the process queue
+ * @param h pointer to the result
+ */
+void hpf_pe(process_queue_t *pq, history_t *h);
+
+
+
+
+#endif //COEN383_P2_SCHEDULER_H
