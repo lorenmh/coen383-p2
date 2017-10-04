d6f5cb0feda701de3f54920a44f786fd2ef476a2
yzhang <azraelexia@gmail.com>
2017-10-03 21:27:15 -0700

    refined the FCFS scheduler

--- Scheduler.c
+++ Scheduler.c
@@ -22,18 +22,6 @@ void fcfs(process_queue_t *pq, history_t *h) {
             }
             current_quanta = end_of_idle;
         }
-        char process_identifier;
-
-
-        if (current_process->id < 26) {
-            process_identifier = (char)((int)'A' + current_process->id);
-        }else if (current_process < 52) {
-            process_identifier = (char)((int)'a' + current_process->id - 26);
-        }else {
-            // we cant use letter to represent new process
-            // because it would collide with existing process's identifier
-            process_identifier = '1';
-        }
 
         current_process->response_time = current_process->arrival_time - current_quanta;
         current_process->turnaround_time = current_process->response_time + current_process->expected_run_time;
@@ -42,7 +30,7 @@ void fcfs(process_queue_t *pq, history_t *h) {
         for (uint32_t j = current_quanta; j < end_of_exec; ++j) {
             // if we could manage to limit the total number of process, we could
             // use A - Z and a - z to represent our process ID
-            buff_for_history[history_size] = process_identifier;
+            buff_for_history[history_size] = current_process->id;
             history_size += 1;
         }
     }
