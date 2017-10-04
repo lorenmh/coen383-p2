07c8fe1481dd537545d1ab3e70b81816c4a5260b
loren <neutronstar@gmail.com>
2017-10-03 23:06:33 -0700

    rng uses rand now

--- RandomNumberGenerator.c
+++ RandomNumberGenerator.c
@@ -1,25 +1,18 @@
 #include "RandomNumberGenerator.h"
 
-#ifdef LINUX
-#include <bsd/stdlib.h>
-#endif
-
-#ifndef LINUX
 #include <stdlib.h>
-#endif
 
 uint32_t RandNum_seed = 0;
 uint32_t RandNum_start = 0;
 uint32_t RandNum_end = RAND_MAX;
 
 void RandNum_set_parameter(int new_seed, int start_value, int end_value) {
-    RandNum_seed = (uint32_t)new_seed;
+    srand(new_seed);
     RandNum_start = (uint32_t)start_value;
     RandNum_end = (uint32_t)end_value;
 }
 
 
 uint32_t RandNum_get_random(void) {
-    uint32_t upperBound = RandNum_end - RandNum_start + 1;
-    return arc4random_uniform(upperBound) + RandNum_start;
+    return (rand() % (RandNum_end + 1)) + RandNum_start;
 }
