b1fb8e9a2a28d48914ad67e79fa4bcc0dd9a500c
Ken Wakaba <kwakaba@scu.edu>
2017-10-05 16:39:18 -0700

    trial rr

--- Scheduler.c
+++ Scheduler.c
@@ -265,7 +265,7 @@ void rr(process_queue_t *pq, history_t *h) {
 
 
         if(current_process->remaining_run_time == 0 && current_process->completed_flag != 1){
-            current_process->turnaround_time = current_quanta - current_process->arrival_time;
+            current_process->turnaround_time = current_quanta - current_process->response_time;
             current_process->completed_flag = 1;
             remaining_processes--;
         }
