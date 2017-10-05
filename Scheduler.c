d05871a8896c50f229d3a7691dcaca076f5f00d6
Ken Wakaba <kwakaba@scu.edu>
2017-10-05 14:40:50 -0700

    trial rr

--- Scheduler.c
+++ Scheduler.c
@@ -268,9 +268,14 @@ void rr(process_queue_t *pq, history_t *h) {
         }
         if(current_process->completed_flag != 1 && current_process->remaining_run_time != 0){
             if(current_process->arrival_flag == 0){
-                current_process->response_time = current_quanta - current_process->arrival_time;
+                if(process_queue_index == 0){
+                    current_process->response_time = current_quanta - current_process->arrival_time;
+                }
+                else{
+                    current_process->response_time++;
+                }
+
                 current_process->arrival_flag = 1;
-                current_quanta++;
             }
             buff_for_history[history_size] = current_process->id;       
             history_size += 1;
