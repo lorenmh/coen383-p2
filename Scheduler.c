0eefe4b57c3d56e6fba87eb4371cfa9fc8916a76
Ken Wakaba <kwakaba@scu.edu>
2017-10-08 15:50:10 -0700

    trial rr

--- Scheduler.c
+++ Scheduler.c
@@ -272,6 +272,10 @@ if (h == NULL) {
             current_process->completed_flag = 1;
         }
         else if(current_process->remaining_run_time > 0){
+            if(current_process->arrival_flag == 0){
+                current_process->response_time = count;
+                current_process->arrival_flag = 1;
+            }
             buff_for_history[history_size] = current_process->id;       
             history_size += 1;
             current_process->remaining_run_time-=time_slice;
