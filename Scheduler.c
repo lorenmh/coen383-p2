45d3831914dd864fadc22daa9f29294790cebb90
Ken Wakaba <kwakaba@scu.edu>
2017-10-08 17:17:59 -0700

    trial rr

--- Scheduler.c
+++ Scheduler.c
@@ -280,9 +280,7 @@ void rr(process_queue_t *pq, history_t *h) {
             time+=current_process->remaining_run_time;
             current_process->remaining_run_time = 0;
             current_process->completed_flag = 1;
-            remaining_processes--;
             current_quanta += 1;
-
         }
         else if(current_process->remaining_run_time > 0){
             if(current_process->arrival_flag == 0){
