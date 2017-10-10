1f01688d6a5e83f4f098efb1457b65432b0c4cfa
yzhang <azraelexia@gmail.com>
2017-10-09 21:15:45 -0700

    generic type of scheduler

--- Scheduler.c
+++ Scheduler.c
@@ -11,6 +11,114 @@
 #define PREEMPT_QUANTUM 5
 #define AGING_QUTANTUM 5
 
+
+void Scheduler(process_queue_t *pq, history_t *h, uint32_t (*key_policy)(process_t *), uint32_t (*interrupt_policy)(uint32_t, uint32_t, uint32_t, uint32_t), bool with_aging) {
+    if (pq == NULL || pq->entry == NULL || h == NULL) {
+        return;
+    }
+
+    char buff_for_history[MAX_BUFF_SIZE] = {0};
+    int history_size = 0;
+
+    for (int idle_time = 0; idle_time < (pq->entry)[0].arrival_time; ++idle_time) {
+        buff_for_history[history_size] = '0';
+        history_size += 1;
+    }
+    uint32_t current_quanta = (pq->entry)[0].arrival_time;
+    int process_index = 0;
+
+    heap_t *process_pool = create_heap();
+
+
+    while (true) {
+        if (current_quanta > 100 && is_empty(process_pool)) {
+            break;
+        }
+        while (process_index < pq->size && (pq->entry)[process_index].arrival_time <= current_quanta) {
+            if (current_quanta > 99) {
+                break;
+            }
+            process_t *new_process = &((pq->entry)[process_index]);
+            uint32_t key = key_policy(new_process);
+            insert(process_pool, key, (void*)new_process);
+            process_index += 1;
+        }
+        process_t *current_process = NULL;
+
+        while (!is_empty(process_pool)) {
+            current_process = extract(process_pool);
+            if (current_quanta > 99 && current_process->response_time == INT32_MAX) {
+                // this process has not gotten its first cpu before quanta 100
+                // just ignore it
+                current_process = NULL;
+                continue;
+            }else {
+                break;
+            }
+        }
+        if (current_process == NULL) {
+            if (current_quanta > 99) {
+                continue;
+            }
+            uint32_t end_of_idle = 99;
+            if (process_index < pq->size) {
+                end_of_idle = (pq->entry)[process_index].arrival_time;
+            }
+            for (int idle_time = current_quanta; idle_time < end_of_idle; ++idle_time) {
+                buff_for_history[history_size] = '0';
+                history_size += 1;
+            }
+            current_quanta = end_of_idle;
+            continue;
+        }
+        if (current_process->response_time == INT32_MAX) {
+            current_process->response_time = current_quanta - current_process->arrival_time;
+        }
+
+
+        uint32_t current_process_remaining_time = current_process->expected_run_time - current_process->execution_time;
+        uint32_t next_interrupt_time = current_quanta + current_process_remaining_time;
+        if (process_index < pq->size) {
+            uint32_t next_arrival = (pq->entry)[process_index].arrival_time;
+            next_interrupt_time = interrupt_policy(current_quanta, current_process_remaining_time, next_arrival, PREEMPT_QUANTUM);
+        }else {
+            next_interrupt_time = interrupt_policy(current_quanta, current_process_remaining_time, UINT32_MAX, PREEMPT_QUANTUM);
+        }
+        for (uint32_t i = current_quanta; i < next_interrupt_time; ++i) {
+            buff_for_history[history_size] = current_process->id;
+            history_size += 1;
+        }
+        uint32_t current_process_running_time = next_interrupt_time - current_quanta;
+        current_process->execution_time += current_process_running_time;
+        current_process_remaining_time -= current_process_running_time;
+
+        current_quanta = next_interrupt_time;
+        if (current_process_remaining_time > 0) {
+            insert(process_pool, current_process_remaining_time, (void*)current_process);
+        }else {
+            current_process->turnaround_time = current_quanta - current_process->arrival_time;
+        }
+
+        if (with_aging) {
+            // too bad!!!
+            // need a "forEach" function support in miniHeap structure
+            //
+
+        }
+
+
+    }
+
+    h->pid = malloc(sizeof(char) * (history_size + 1));
+    memcpy(h->pid, buff_for_history, sizeof(char) * history_size);
+    (h->pid)[history_size] = '\0';
+    h->size = history_size;
+
+    // clean up
+    free_heap(process_pool);
+}
+
+
 void fcfs(process_queue_t *pq, history_t *h) {
     if (h == NULL) {
         return;
@@ -404,7 +512,7 @@ void hpf_npe(process_queue_t *pq, history_t *h, bool with_aging) {
         if (with_aging) {
             // too bad!!!
             // need a "forEach" function support in miniHeap structure
-            // 
+            //
 
         }
 
